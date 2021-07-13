57da6a2a962541c366942b4c556af1202e5f171c
Freyr Guðjónsson <f@sensa.is>
2021-07-13 11:11:39 +0000

    Update readme with vlan_id instead of encap

--- README.md
+++ README.md
@@ -38,12 +38,12 @@ module "aci_tenant" {
       domains             = ["uni/phys-MY_PHYSICAL_DOMAIN"]
       static_paths        = [
         {
-          encap = 100
-          path = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
+          vlan_id = 100
+          path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
         },
         {
-          encap = 100
-          path = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
+          vlan_id = 100
+          path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
         }
       ]
     }
--- main.tf
+++ main.tf
@@ -39,12 +39,12 @@
  *       domains             = ["uni/phys-MY_PHYSICAL_DOMAIN"]
  *       static_paths        = [
  *         {
- *           encap = 100
- *           path = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
+ *           vlan_id = 100
+ *           path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_A]"
  *         },
  *         {
- *           encap = 100
- *           path = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
+ *           vlan_id = 100
+ *           path    = "topology/pod-1/protpaths-201-202/pathep-[MY_VPC_PATH_B]"
  *         }
  *       ]
  *     }
